e44ca30113309d085aa20ab6b1f79042a6437b60
Holger <hdecarne@gmail.com>
2018-12-22 12:56:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Common test code shared between projects
+[![Download](https://api.bintray.com/packages/hdecarne/maven/java-test/images/download.svg)](https://bintray.com/hdecarne/maven/java-test/_latestVersion)
+[![Build Status](https://travis-ci.com/hdecarne/java-test.svg?branch=master)](https://travis-ci.com/hdecarne/java-test)
+[![Quality Gate](https://sonarcloud.io/api/project_badges/measure?project=de.carne.common%3Ajava-test%3Ajava-test&metric=alert_status)](https://sonarcloud.io/dashboard/index/de.carne.common:java-test:java-test)
+
+#### License
+This project is subject to the the GNU General Public License version 3 or later version.
+See LICENSE information for details.
---
+++ src/test/java/de/carne/test/test/extension/TempPathParameterResolverTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test.test.extension;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import de.carne.nio.file.FileUtil;
+import de.carne.test.extension.TempPath;
+import de.carne.test.extension.TempPathParameterResolver;
+
+/**
+ * Test {@linkplain TempPathParameterResolver} class.
+ */
+@ExtendWith(TempPathParameterResolver.class)
+// TODO: Set fixed test order with JUnit 5.4.0
+class TempPathParameterResolverTest {
+
+	private static final String TEST_FILE1 = "file1.tmp";
+
+	@Test
+	void test1stAccess(TempPath tempPath) {
+		Path testFile1 = tempPath.get().resolve(TEST_FILE1);
+
+		Assertions.assertTrue(Files.exists(testFile1, LinkOption.NOFOLLOW_LINKS));
+	}
+
+	@Test
+	void test2ndtAccess(TempPath tempPath) throws IOException {
+		Path testFile1 = tempPath.get().resolve(TEST_FILE1);
+
+		Assertions.assertFalse(Files.exists(testFile1, LinkOption.NOFOLLOW_LINKS));
+
+		FileUtil.touch(testFile1);
+
+		Assertions.assertTrue(Files.exists(testFile1, LinkOption.NOFOLLOW_LINKS));
+	}
+
+}
